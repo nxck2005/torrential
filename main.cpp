489b2626f92fe2b178d3f13287ae770a601aac0e
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-01-12 20:26:49 +0530

    Goofing around to be honest

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ int main(int argc, char ** argv) {
     // initialises screen
     // sets up memory and clears the screen
     initscr();
+    raw();
+    noecho();
+
+    int x, y;
+    
+    x = y = 10;
+
+    // Move cursor to 0,0 (y, x)
+    move(y, x);
 
     // Prints a string (const char *) to a window
     printw("torrential 0.0.0.1");
@@ -13,6 +22,13 @@ int main(int argc, char ** argv) {
     refresh();
 
     // Wait for us to press a key
+    int c = getch();
+
+    // clears the screen
+    clear();
+
+    mvprintw(0, 0, "%c", c);
+
     getch();
 
     // Deallocates memory and ends ncurses
