ebf44b49a8488478ec52280a2d38eb5867c26eed
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-01-15 00:57:09 +0530

    Add content window, adjust dimensions and timeout for getch in main

--- ui/uihandler.hpp
+++ ui/uihandler.hpp
@@ -8,6 +8,7 @@ extern volatile sig_atomic_t resize_needed;
 // All your function declarations
 void drawTitle();
 void drawMenu();
+void drawContent();
 void drawAllWindows();
 void handleResize(int sig);
 void doResize();
